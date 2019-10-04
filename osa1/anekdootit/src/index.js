a5505fbacb4a2fec9b57e76818ddae7c7450c6ad
samuvait <sav.v@hotmail.fi>
2019-10-04 21:02:08 +0300

    Exercise 1.14

--- osa1/anekdootit/src/index.js
+++ osa1/anekdootit/src/index.js
@@ -4,6 +4,7 @@ import ReactDOM from 'react-dom'
 const App = (props) => {
   const [selected, setSelected] = useState(0)
   const [amount, setAmount] = useState(0)
+  const [mostVotes, setMost] = useState(0)
 
   const handleClick = () => {
     rnd.value = Math.floor(Math.random() * 6)
@@ -17,21 +18,36 @@ const App = (props) => {
     votes[rnd.value] += 1
     console.log('amount after', votes[rnd.value])
     setAmount(votes[rnd.value])
+    mostVoted()
+  }
+
+  const mostVoted = () => {
+    let most = 0
+    for (let i = 0; i < votes.length; i++) {
+      if (votes[i] > votes[most]) {
+        most = i
+      }
+    }
+    console.log('most votes', most)
+    setMost(most)
   }
 
   return (
     <div>
-      <button onClick={handleVote}>Vote</button>
-      <button onClick={handleClick}>Next anecdote</button>
-      <p>{props.anecdotes[selected]}</p>
-      <p>This anecdote has {amount} votes</p>
+      <div>
+        <h2>Anecdote of the Day</h2>
+        <p>{props.anecdotes[selected]}</p>
+        <p>This anecdote has {amount} votes</p>
+        <button onClick={handleVote}>Vote</button>
+        <button onClick={handleClick}>Next anecdote</button>
+      </div>
+      <div>
+        <h2>Anecdote with most votes</h2>
+        <p>{props.anecdotes[mostVotes]}</p>
+      </div>
     </div>
   )
 }
-
-const rnd = {"value": 0}
-
-const votes = new Array(6).fill(0)
 
 const anecdotes = [
   'If it hurts, do it more often',
@@ -41,6 +57,10 @@ const anecdotes = [
   'Premature optimization is the root of all evil.',
   'Debugging is twice as hard as writing the code in the first place. Therefore, if you write the code as cleverly as possible, you are, by definition, not smart enough to debug it.'
 ]
+
+const rnd = { "value": 0 }
+
+const votes = new Array(anecdotes.length).fill(0)
 
 ReactDOM.render(
   <App anecdotes={anecdotes} />,
