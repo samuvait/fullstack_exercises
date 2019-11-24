606d14bf819e2b3d1090d216c2bc631654a21b2f
samuvait <sav.v@hotmail.fi>
2019-11-24 17:34:08 +0200

    Exercise 4.4

--- osa4/bloglist/utils/list_helper.js
+++ osa4/bloglist/utils/list_helper.js
@@ -2,6 +2,15 @@ const dummy = (blogs) => {
   return 1
 }
 
+const totalLikes = (blogs) => {
+  return blogs.length === 0
+    ? 0
+    : blogs.reduce((sum, blog) => {
+      return sum += blog.likes
+    }, 0)
+}
+
 module.exports = {
-  dummy
+  dummy,
+  totalLikes
 }
