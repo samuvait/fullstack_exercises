7b772e724edb1efe9810185e19c022c03b9a2b49
samuvait <sav.v@hotmail.fi>
2019-11-27 13:37:15 +0200

    Exercise 4.17

--- osa4/bloglist/controllers/blogs.js
+++ osa4/bloglist/controllers/blogs.js
@@ -1,23 +1,38 @@
 const blogsRouter = require('express').Router()
 const Blog = require('../models/blog')
+const User = require('../models/user')
 
 blogsRouter.get('/', async (request, response) => {
   const blogs = await Blog.find({})
-  response.json(blogs.map(blog => blog.toJSON()))
+    .populate('user', {username: 1, name: 1})
+
+  try {
+    response.json(blogs.map(blog => blog.toJSON()))
+  } catch (error) {
+    response.status(400).json({ error: error.message })
+  }
 })
 
 blogsRouter.post('/', async (request, response) => {
   const blog = request.body
-  const newBlog = new Blog({
-    title: blog.title,
-    author: blog.author,
-    url: blog.url,
-    likes: blog.likes || 0
-  })
+  console.log('blog', blog)
 
   try {
-    const saved = await newBlog.save()
-    response.status(201).json(saved.toJSON())
+    const user = await User.findById(blog.user)
+    console.log('user', user)
+
+    const newBlog = new Blog({
+      title: blog.title,
+      author: blog.author,
+      url: blog.url,
+      likes: blog.likes || 0,
+      user: user._id
+    })
+
+    const savedBlog = await newBlog.save()
+    user.blogs = user.blogs.concat(savedBlog._id)
+    await user.save()
+    response.status(201).json(savedBlog.toJSON())
   } catch (error) {
     response.status(400).json({ error: error.message })
   }
@@ -43,7 +58,7 @@ blogsRouter.put('/:id', async (request, response) => {
   })
 
   try {
-    const updatedBlog = await Blog.findOneAndUpdate({ _id: id }, {likes: newBlog.likes}, { new: true })
+    const updatedBlog = await Blog.findOneAndUpdate({ _id: id }, { likes: newBlog.likes }, { new: true })
     response.json(updatedBlog.toJSON())
   } catch (error) {
     response.status(400).json({ error: error.message })
--- osa4/bloglist/controllers/users.js
+++ osa4/bloglist/controllers/users.js
@@ -31,6 +31,8 @@ usersRouter.post('/', async (request, response, next) => {
 usersRouter.get('/', async (request, response, next) => {
   try {
     const users = await User.find({})
+      .populate('blogs', { title: 1, author: 1, url: 1})
+
     response.json(users.map(user => user.toJSON()))
   } catch (exception) {
     response.status(400).json({ error: error.message })
